c172c0f0d8226e1d01f7a1d7a95ee4a651e9e8f6
alex <alex.kimani@compulynx.org>
2014-12-22 17:56:11 +0300

    code clean up: clearing errors

--- src/main/java/org/compulynx/enterprise/core/servicelayer/rest/Configuration.java
+++ src/main/java/org/compulynx/enterprise/core/servicelayer/rest/Configuration.java
@@ -1,62 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package org.compulynx.enterprise.core.servicelayer.rest;
-
-import java.util.List;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import org.compulynx.enterprise.core.businesslogiclayer.models.hierarchy.Field;
-import org.compulynx.enterprise.core.businesslogiclayer.models.hierarchy.Item;
-import org.compulynx.enterprise.core.businesslogiclayer.models.hierarchy.OrgHierarchy;
-import org.compulynx.enterprise.core.businesslogiclayer.models.hierarchy.Structure;
-import org.compulynx.enterprise.core.servicelayer.DummyDataGenerator;
-
-/**
- *
- * @author Kimando
- */
-
-@Path("/config")
-public class Configuration
-{
-    @POST
-    @Path("/create")
-    @Consumes("application/json")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response createConfig(OrgHierarchy orgHierarchy)
-    {    
-        List<Structure> structures = orgHierarchy.getStructures();
-        
-        org.compulynx.enterprise.core.businesslogiclayer.controllers.hierarchy.Configuration configuration = new org.compulynx.enterprise.core.businesslogiclayer.controllers.hierarchy.Configuration();
-        
-        return Response.status(200).entity(configuration.createOrganisationHierarchy(orgHierarchy)).build();
-    }
-    
-    @POST
-    @Path("/get")
-    @Consumes("application/json")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getConfig()
-    {  
-        /*DummyDataGenerator dummyDataGenerator = new DummyDataGenerator();
-        
-        List<Field> fields = dummyDataGenerator.generateFields(1,3);
-        List<Item> items = dummyDataGenerator.generateItems(4, 7, fields);
-        List<Structure> structures = dummyDataGenerator.generateStructures(1, 3, fields, items);
-        
-        OrgHierarchy orgHierarchy = new OrgHierarchy();
-        orgHierarchy.setStructures(structures);*/
-        
-        org.compulynx.enterprise.core.businesslogiclayer.controllers.hierarchy.Configuration configuration = new org.compulynx.enterprise.core.businesslogiclayer.controllers.hierarchy.Configuration();
-        
-        return Response.status(200).entity(configuration.fetchOrganisationHierarchy()).build();
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//package org.compulynx.enterprise.core.servicelayer.rest;
+//
+//import java.util.List;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//import org.compulynx.enterprise.core.businesslogiclayer.models.hierarchy.Field;
+//import org.compulynx.enterprise.core.businesslogiclayer.models.hierarchy.Item;
+//import org.compulynx.enterprise.core.businesslogiclayer.models.hierarchy.OrgHierarchy;
+//import org.compulynx.enterprise.core.businesslogiclayer.models.hierarchy.Structure;
+//import org.compulynx.enterprise.core.servicelayer.DummyDataGenerator;
+//
+///**
+// *
+// * @author Kimando
+// */
+//
+//@Path("/config")
+//public class Configuration
+//{
+//    @POST
+//    @Path("/create")
+//    @Consumes("application/json")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response createConfig(OrgHierarchy orgHierarchy)
+//    {
+//        List<Structure> structures = orgHierarchy.getStructures();
+//
+//        org.compulynx.enterprise.core.businesslogiclayer.controllers.hierarchy.Configuration configuration = new org.compulynx.enterprise.core.businesslogiclayer.controllers.hierarchy.Configuration();
+//
+//        return Response.status(200).entity(configuration.createOrganisationHierarchy(orgHierarchy)).build();
+//    }
+//    
+//    @POST
+//    @Path("/get")
+//    @Consumes("application/json")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response getConfig()
+//    {
+//        /*DummyDataGenerator dummyDataGenerator = new DummyDataGenerator();
+//
+//        List<Field> fields = dummyDataGenerator.generateFields(1,3);
+//        List<Item> items = dummyDataGenerator.generateItems(4, 7, fields);
+//        List<Structure> structures = dummyDataGenerator.generateStructures(1, 3, fields, items);
+//
+//        OrgHierarchy orgHierarchy = new OrgHierarchy();
+//        orgHierarchy.setStructures(structures);*/
+//
+//        org.compulynx.enterprise.core.businesslogiclayer.controllers.hierarchy.Configuration configuration = new org.compulynx.enterprise.core.businesslogiclayer.controllers.hierarchy.Configuration();
+//
+//        return Response.status(200).entity(configuration.fetchOrganisationHierarchy()).build();
+//    }
+//}
